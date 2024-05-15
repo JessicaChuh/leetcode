ad03e4c2c6f5f4ea1258f1ac4e8fd4c30ae93fb1
Jessica Chuh <Jessica_Chuh@yahoo.com>
2024-05-15 21:16:55 +0800

    still quite confusing

--- 605_canPlaceFlowers.py
+++ 605_canPlaceFlowers.py
@@ -11,9 +11,16 @@ n = 3
 Output: True
 
 def canPlaceFlowers(flowerbed, n):
-    if flowerbed.count(0)//3 > n:
-        return True
-    else:
-        return False
+    length = len(flowerbed)
+    count = 0
+    i = 0
 
-print(canPlaceFlowers(flowerbed, n))
+    while i < length:
+        if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == length - 1 or flowerbed[i + 1] == 0):
+            flowerbed[i] = 1
+            count += 1
+            if count >= n:
+                return True
+        i += 1
+
+    return False
