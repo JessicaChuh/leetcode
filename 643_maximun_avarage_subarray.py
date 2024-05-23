a25285e2da38836797e9a0bf930a73047124331a
Jessica Chuh <Jessica_Chuh@yahoo.com>
2024-05-23 15:17:27 +0800

    over time

--- 643_maximun_avarage_subarray.py
+++ 643_maximun_avarage_subarray.py
@@ -5,5 +5,15 @@
 # average value and return this value. Any answer with a calculation error
 # less than 10-5 will be accepted.
 
+nums = [1,12,-5,-6,50,3]
+k = 4
 
 def findMaxAverage(nums,k):
+    left = 0
+    ans = []
+    while left + k <= len(nums):
+        ans.append(sum(nums[left:left+k])/k)
+        left += 1
+    return round(max(ans),5)
+
+print(findMaxAverage(nums,k))
