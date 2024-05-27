0e38552395c4887e4740556b6520c426a070123f
Jessica Chuh <Jessica_Chuh@yahoo.com>
2024-05-27 22:36:45 +0800

    sliding windows

--- 1104_max_consecutive_ones.py
+++ 1104_max_consecutive_ones.py
@@ -4,3 +4,23 @@
 
 nums = [1,1,1,0,0,0,1,1,1,1,0]
 k = 2
+
+def longestOnes(nums,k):
+    left, right = 0,0
+    max_zeros = 0
+    flips = 0
+
+    while right < len(nums):
+        if nums[right] == 0:
+            flips += 1
+
+        while flips > k:
+            if nums[left] == 0:
+                flips -= 1
+            left += 1
+
+        max_zeros = max(max_zeros,right-left+1)
+        right += 1
+    return max_zeros
+
+print(longestOnes(nums,k))
