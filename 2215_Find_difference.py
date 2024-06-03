3d1f636b37aad1ae8cc55546c4ff34b8b9528759
Jessica Chuh <Jessica_Chuh@yahoo.com>
2024-06-03 17:54:07 +0800

    sets can be subtracted

--- 2215_Find_difference.py
+++ 2215_Find_difference.py
@@ -8,3 +8,20 @@
 
 #Note that the integers in the lists may be returned in any
 #order.
+
+nums1 = [1,2,3,3]
+nums2 = [1,1,2,2]
+
+set1 = set(nums1)
+set2 = set(nums2)
+
+diff1 = list(set1 - set2)
+diff2 = list[set2 - set1]
+
+
+print([diff1,diff2])
+
+
+#TakeAway:
+#subtraction operation doesn't work between list but works between
+#sets.
