8b3c96037ec7ba3ca053067c97a860ac3c877b9a
Jessica Chuh <Jessica_Chuh@yahoo.com>
2024-06-04 13:56:55 +0800

    Hash table

--- 1207_uniqueOccurrences.py
+++ 1207_uniqueOccurrences.py
@@ -5,11 +5,20 @@
 arr = [1,2,2,1,1,3,4]
 
 def uniqueOccurrences(arr):
-    new = set(arr)
-    ans = []
-    for i in new:
-        ans.append(arr.count(i))
+#     new = set(arr)
+#     ans = []
+#     for i in new:
+#         ans.append(arr.count(i))
 
-    return True if len(ans) == len(set(ans)) else False
+#     return True if len(ans) == len(set(ans)) else False
+
+# print(uniqueOccurrences(arr))
+
+#Hash table
+    counts = {}
+    for i in arr:
+        counts[i] = arr.count(i)
+
+    return True if len(counts.values()) == len(set(counts.values())) else False
 
 print(uniqueOccurrences(arr))
