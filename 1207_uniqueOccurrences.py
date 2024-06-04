d3863a15a8e826a6cff6f15ee574b1c821c7a28e
Jessica Chuh <Jessica_Chuh@yahoo.com>
2024-06-04 13:46:24 +0800

    Time and space complexity O(n)

---
+++ 1207_uniqueOccurrences.py
@@ -0,0 +1,15 @@
+#Given an array of integers arr, return true if the number of
+#occurrences of each value in the array is unique or false
+#otherwise.
+
+arr = [1,2,2,1,1,3,4]
+
+def uniqueOccurrences(arr):
+    new = set(arr)
+    ans = []
+    for i in new:
+        ans.append(arr.count(i))
+
+    return True if len(ans) == len(set(ans)) else False
+
+print(uniqueOccurrences(arr))
