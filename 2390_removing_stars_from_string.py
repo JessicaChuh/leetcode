094ca5dbb150c3696b8ecf1a1457d9874d738750
Jessica Chuh <Jessica_Chuh@yahoo.com>
2024-06-12 13:51:50 +0800

    time complecity O(n^2)

--- 2390_removing_stars_from_string.py
+++ 2390_removing_stars_from_string.py
@@ -3,10 +3,26 @@
 #In one operation, you can:
 
 #Choose a star in s.
-# Remove the closest non-star character to its left, as well as remove the star itself.
+# Remove the closest non-star character to its left, as well as
+# remove the star itself.
 # Return the string after all stars have been removed.
 
 # Note:
 
 # The input will be generated such that the operation is always possible.
 # It can be shown that the resulting string will always be unique.
+
+#first try
+
+def removeStars(self, s: str) -> str:
+        s = list(s)
+        for i in range(s.count('*')):
+            if s.index('*') > 0:
+                del s[s.index('*')-1:s.index('*')+1]
+            else:
+                del s[s.index('*')]
+
+        return ''.join(s)
+
+#time complexity is O(n^2), because index() is looping through
+# the whole list of
